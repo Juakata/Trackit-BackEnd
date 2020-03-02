01aa8095a960b445bfe1ad074a3e388ea73cf297
Juakata <juakata07@gmail.com>
2020-03-02 12:04:59 -0600

    add user model, controller and tests

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::MoviesController < Api::V1::ApiController
+  before_action :set_user, only: %i[show update destroy]
+
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @movie.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+    def set_movie
+      @user = User.find(params[:id])
+    end
+
+    def movie_params
+      params.require(:movie).permit(:username, :password)
+    end
+end
