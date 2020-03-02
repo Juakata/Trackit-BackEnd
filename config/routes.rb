8862a4404551a52c3a293f6098ac2072db966856
Juakata <juakata07@gmail.com>
2020-03-02 15:59:52 -0600

    Force all access to the app over SSL

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
       resources :users
     end
   end
+  # namespace :api, :path => "", :constraints => {:subdomain => "api"} do
+  #   namespace :v1 do
+  #     resources :users
+  #   end
+  # end
 end
