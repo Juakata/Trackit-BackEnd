72473e4c0ec75843a0bb6cfd5ac6374cff1aa057
Juakata <juakata07@gmail.com>
2020-03-02 17:05:34 -0600

    remove features from link

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api,
-            :path => "",
-            :constraints => {:subdomain => "api"},
-            :defaults => {:format => :json} do
+  namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
       resources :users
       get '/login/:username/:password', to: 'sessions#create'
