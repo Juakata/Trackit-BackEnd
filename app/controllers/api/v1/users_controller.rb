27761f859dfd281c53fec529060932c727c2a620
Juakata <juakata07@gmail.com>
2020-03-03 13:37:25 -0600

    add create user link

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,12 +14,15 @@ class Api::V1::UsersController < Api::V1::ApiController
   end
 
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created
+    if params[:password] === params[:repeat]
+      @user = User.new(username: params[:username], password: params[:password])
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     else
-      render json: @user.errors, status: :unprocessable_entity
+      return { result: 'Passwords do not match.'}
     end
   end
 
