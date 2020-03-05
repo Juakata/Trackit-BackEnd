31c05e3b260fe315e97c1891e5a9f1908f43724c
Juakata <juakata07@gmail.com>
2020-03-05 16:06:05 -0600

    fix issue when finding a category

--- app/controllers/api/v1/user_categories_controller.rb
+++ app/controllers/api/v1/user_categories_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UserCategoriesController < Api::V1::ApiController
   def setUserCategory
     @usercategory = UserCategory.find_by(date: params[:date])
     @user_id = User.find_by(username: params[:user]);
-    @category_id = Category.find_by(category_id: params[:category])
+    @category_id = Category.find_by(name: params[:category])
     if @usercategory
       newProgress = @usercategory.progress + params[:progress].to_i
       @usercategory.update(progress: newProgress)
