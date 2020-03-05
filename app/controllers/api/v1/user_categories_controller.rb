fac5b9751629324e008354936b27a1191cff52c5
Juakata <juakata07@gmail.com>
2020-03-05 16:29:16 -0600

    add render json result

--- app/controllers/api/v1/user_categories_controller.rb
+++ app/controllers/api/v1/user_categories_controller.rb
@@ -8,12 +8,14 @@ class Api::V1::UserCategoriesController < Api::V1::ApiController
     if @usercategory
       newProgress = @usercategory.progress + params[:progress].to_i
       @usercategory.update(progress: newProgress)
+      render json: { result: "updated" }
     else
       @usercategory = UserCategory.new(date: params[:date],
                                       progress: params[:progress],
                                       user_id: @user_id.id,
                                       category_id: @category_id.id)
       @usercategory.save
+      render json: { result: "created" }
     end
   end
 end
