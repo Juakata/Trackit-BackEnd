78c2d672efc42bccadfbfc02293cf31ccaf91fa4
Juakata <juakata07@gmail.com>
2020-03-05 17:35:24 -0600

    look relation user-cat also for category_id

--- app/controllers/api/v1/user_categories_controller.rb
+++ app/controllers/api/v1/user_categories_controller.rb
@@ -2,9 +2,10 @@
 
 class Api::V1::UserCategoriesController < Api::V1::ApiController
   def setUserCategory
-    @usercategory = UserCategory.find_by(date: params[:date])
-    @user_id = User.find_by(username: params[:user]);
     @category_id = Category.find_by(name: params[:category])
+    @usercategory = UserCategory.find_by(date: params[:date], category_id: @category_id)
+    @user_id = User.find_by(username: params[:user])
+
     if @usercategory
       newProgress = @usercategory.progress + params[:progress].to_i
       @usercategory.update(progress: newProgress)
