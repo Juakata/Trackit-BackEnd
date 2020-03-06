465824d9090af1cd0303b01e3283ec9b59638841
Juakata <juakata07@gmail.com>
2020-03-05 19:44:05 -0600

    fix issue with data for other user not being saved

--- app/controllers/api/v1/user_categories_controller.rb
+++ app/controllers/api/v1/user_categories_controller.rb
@@ -4,8 +4,10 @@ class Api::V1::UserCategoriesController < Api::V1::ApiController
   def setUserCategory
     @category = fixString(params[:category])
     @category_id = Category.find_by(name: @category)
-    @usercategory = UserCategory.find_by(date: params[:date], category_id: @category_id)
     @user_id = User.find_by(username: params[:user])
+    @usercategory = UserCategory.find_by(date: params[:date],
+                                        category_id: @category_id,
+                                        user_id: @user_id)
 
     if @usercategory
       newProgress = @usercategory.progress + params[:progress].to_i
