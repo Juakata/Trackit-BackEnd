15768ac86fb007d6d598f931f6c113c0d917837d
Juakata <juakata07@gmail.com>
2020-03-06 17:51:27 -0600

    fix key word

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::CategoriesController < Api::V1::ApiController
     @user = User.find_by(username: params[:username])
     if @user
       @user.categories.find_by(name: "Networking").update(goal_time: params[:net])
-      @user.categories.find_by(name: "Look for job").update(goal_time: params[:look])
+      @user.categories.find_by(name: "Looking for job").update(goal_time: params[:look])
       @user.categories.find_by(name: "Coding Challenges").update(goal_time: params[:cod])
       @user.categories.find_by(name: "Relaxing").update(goal_time: params[:rel])
     end
