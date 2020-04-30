2e2fa96e922f1d98485bdc5e1a8b7f00433ab6e5
Juakata <juakata07@gmail.com>
2020-04-30 12:03:28 -0600

    pull names instead of ids

--- app/controllers/api/v1/voicemails_controller.rb
+++ app/controllers/api/v1/voicemails_controller.rb
@@ -6,7 +6,18 @@ class Api::V1::VoicemailsController < Api::V1::ApiController
       params[:receiver],
       params[:sender])
     if voicemails
-      render json: { voicemails: voicemails }
+      send = []
+      voicemails.each do |voice|
+        from = Usermail.find(voice.from_id)
+        to = Usermail.find(voice.to_id)
+        send.push({
+          id: voice.id,
+          status: voice.status,
+          from: from.name,
+          to: to.name,
+          duration: voice.duration})
+      end
+      render json: { messages: send }
     else
       render json: { result: 'Not results' }
     end
