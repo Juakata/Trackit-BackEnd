55ea6f21a30d35926ae86a3dffda6a885f0a374e
Juakata <juakata07@gmail.com>
2020-04-30 13:22:10 -0600

    add another request

--- app/controllers/api/v1/voicemails_controller.rb
+++ app/controllers/api/v1/voicemails_controller.rb
@@ -22,4 +22,21 @@ class Api::V1::VoicemailsController < Api::V1::ApiController
       render json: { result: 'Not results' }
     end
   end
+
+  def otro_pull_voicemails
+    voicemails = Voicemail.where('to_id = (?) AND from_id = (?)',
+      params[:receiver],
+      params[:sender])
+    if voicemails
+      send = []
+      voicemails.each do |voice|
+        from = Usermail.find(voice.from_id)
+        to = Usermail.find(voice.to_id)
+        send.push({ id: voice.id, status: voice.status })
+      end
+      render json: { messages: send }
+    else
+      render json: { result: 'Not results' }
+    end
+  end
 end
